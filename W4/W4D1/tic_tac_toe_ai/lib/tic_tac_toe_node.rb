f64e10d88e81d95f48fc435294da3a676e740cbc
Kent Zhang <kentzhang31@yahoo.com>
2021-07-26 16:14:34 -0700

    Implemented losing_node? and winning_node?

--- W4/W4D1/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ W4/W4D1/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -7,25 +7,35 @@ class TicTacToeNode
     @next_mover_mark = next_mover_mark
     @prev_move_pos = prev_move_pos
   end
+  # x o x
+  # o x o
+  # o x
 
   def losing_node?(evaluator)
-    return true if board.over? && (board.winner != evaluator)
-
-    if next_mover_mark != evaluator
-      all_moves = children
-      all_moves.each do |move|
-        return false if !move.losing_node?(evaluator)
-      end
-    else
-      all_moves = children
-      all_moves.each do |move|
-        return true if move.losing_node?(evaluator)
-      end
+    if board.over?
+      return board.winner != evaluator && board.won? # nil != evaluator = true 
     end
-    false
+    
+
+    all_moves = children
+    if next_mover_mark == evaluator
+      all_moves.all? { |move| move.losing_node?(evaluator) }
+    else
+      all_moves.any? { |move| move.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
+    if board.over?
+      return board.winner == evaluator && board.won?
+    end
+    all_moves = children
+    
+    if next_mover_mark == evaluator
+      all_moves.any? { |move| move.winning_node?(evaluator) }
+    else
+      all_moves.all? { |move| move.winning_node?(evaluator) }
+    end
   end
 
   # This method generates an array of all moves that can be made after
@@ -33,25 +43,28 @@ class TicTacToeNode
   def children
     all_moves = []
 
-
     (0...3).each do |row|
       (0...3).each do |col|
         pos = [row, col]
-
-        if next_mover_mark == :x
-          next_mover_mark = :o
-        else
-          next_mover_mark = :x
-        end
 
         if self.board.empty?(pos)
           copy = self.board.dup
           copy[pos] = next_mover_mark
           prev_move_pos = pos
+          if next_mover_mark == :x
+            next_mover_mark = :o
+          else
+            next_mover_mark = :x
+          end
           all_moves << TicTacToeNode.new(copy, next_mover_mark, prev_move_pos)
+
         end
       end
     end
     all_moves
   end
 end
+
+b = Board.new
+t = TicTacToeNode.new(b, :o, :x)
+t.losing_node?(:o)
