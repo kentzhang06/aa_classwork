59034ae0a3f85e575aef040e0523eec3bd429629
josephafern <60451011+josephafern@users.noreply.github.com>
2021-07-27 15:16:04 -0700

    Added bracket getter and setter methods for Board.grid. Added move_piece and swap.

--- W4/W4D2/chess/board.rb
+++ W4/W4D2/chess/board.rb
@@ -2,6 +2,8 @@ require_relative 'piece'
 
 class Board
 
+  attr_accessor :grid
+
   def initialize
     @grid = Array.new(8) { Array.new(8) } #rows 0 1 6 7 have pieces
     rows_to_include = [0, 1, 6, 7]
@@ -23,7 +25,35 @@ class Board
     puts "#{@grid}"
   end
 
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, piece)
+    row, col = pos
+    @grid[row][col] = piece
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise "No piece at start position." if self[start_pos].is_a?(NullPiece)
+    #raise "Piece cannot move to end position."  if valid_move && pos_on_board
+    if self[end_pos].is_a?(NullPiece)
+      swap(start_pos, end_pos)
+    else
+      self[end_pos] = self[start_pos]
+      self[start_pos] = NullPiece.instance
+    end
+  end
+
+  def swap(start_pos, end_pos)
+    self[start_pos], self[end_pos] = self[end_pos], self[start_pos]
+  end
+
 end
 
 b = Board.new
-p b
+p b[[0,0]] #piece
+p b[[2,0]] #np
+p b[[7,0]] #piece
+b.move_piece([2,0], [4,0])
