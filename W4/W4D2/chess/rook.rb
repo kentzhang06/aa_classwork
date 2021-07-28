e01c31c669f1f066d007a9edf14c40cbe132d772
josephafern <60451011+josephafern@users.noreply.github.com>
2021-07-28 14:45:04 -0700

    Finished working version of move_dirs, made progress on grow_unblocked... and defined helped method on_board.

--- W4/W4D2/chess/rook.rb
+++ W4/W4D2/chess/rook.rb
@@ -12,23 +12,13 @@ class Rook < Piece
   def symbol
     @color
   end
-  
-  # protected
-  # def move_dirs
-  # #   # row, col = @pos
-  # #   # dirs = [[row + 1, col], [row, col + 1], [row - 1, col], [row, col - 1]]
-  # #   # res = []
-  # #   # dirs.each_with_index do |dir, i|
-  # #   #   unless (dir[0] < 0 || dir[0] > 7) || (dir[1] < 0 || dir[1] > 7)
-  # #   #     || @board[dir].color == self.color
-  # #   #     res << compass[i]
-  # #   #   end
-  # #   # end
-  # #   # res
-  # end
+
+  def move_dirs
+    horizontal_dirs
+  end
 
 end
 
 b = Board.new
 rook = Rook.new([0, 0], b, :white)
-p rook.move_dirs
+p rook.moves
--- W4/W4D2/chess/slideable.rb
+++ W4/W4D2/chess/slideable.rb
@@ -1,6 +1,6 @@
 module Slideable
-  HORIZONTAL_DIRS = { 'N'=>[-1, 0], 'E'=>[0, -1], 'S'=>[1, 0], 'W'=>[0, 1] }
-  DIAGONAL_DIRS = { 'NE'=>[-1, 1], 'NW'=>[-1, -1], 'SE'=>[1, 1], 'SW'=>[1, -1] }
+  HORIZONTAL_DIRS = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+  DIAGONAL_DIRS = [[-1, 1],[-1, -1],[1, 1],[1, -1]]
 
   def horizontal_dirs
     HORIZONTAL_DIRS
@@ -11,17 +11,36 @@ module Slideable
   end
 
   def moves
-    
+     
   end
 
   private
 
   def move_dirs
-    HORIZONTAL_DIRS.merge(DIAGONAL_DIRS)
+    HORIZONTAL_DIRS.concat(DIAGONAL_DIRS)
   end
 
   def grow_unblocked_moves_in_dir(dx, dy)
+    row, col = pos
+    new_dx, new_dy = row + dx, col + dy
+    curr_piece = board[new_dx, new_dy]
+    while curr_piece.is_a?(NullPiece) && on_board?([new_dx, new_dy]) && color != curr_piece.color
+      
+    end
 
   end
+
+  def on_board?(pos)
+    row, col = pos
+    row >= 0 && row < 8 && col >= 0 col < 8
+  end
+
+  # in a loop:
+      # continually increment the piece's current row and current column to generate a new position
+      # stop looping if the new position is invalid (not on the board); the piece can't move in this direction
+      # if the new position is empty, the piece can move here, so add the new position to the moves array
+      # if the new position is occupied with a piece of the opposite color, the piece can move here (to capture the opposing piece), so add the new position to the moves array
+        # but, the piece cannot continue to move past this piece, so stop looping
+      # if the new position is occupied with a piece of the same color, stop looping
 end
 
