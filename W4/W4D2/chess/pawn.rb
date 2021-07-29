d2dba0c909354425c1d185a3a86a58df35d8d7de
josephafern <60451011+josephafern@users.noreply.github.com>
2021-07-28 17:29:25 -0700

    Completed Chess:Part 1

--- W4/W4D2/chess/pawn.rb
+++ W4/W4D2/chess/pawn.rb
@@ -1,24 +1,24 @@
 require_relative 'piece'
 
-class Pawn
+class Pawn < Piece
   def initialize(pos, board, symbol)
     super
   end
 
   def symbol
-
+    color
   end
 
   def moves
-
+    side_attacks + forward_steps
   end
 
-  private
+  #private
 
   def at_start_row?
     row, col = pos
     piece = board[pos]
-    (piece.color == white && row == 1) || (piece.color == black && row == 6)
+    (piece.color == :white && row == 1) || (piece.color == :black && row == 6)
   end
 
   def forward_dir
@@ -26,11 +26,11 @@ class Pawn
 
     case symbol
     
-    when white
-      result.push([2, 0], [1, 0]) if at_start_row?
+    when :white
+      result.push([2, 0]) if at_start_row?
       result.push([1, 0])
-    when black
-      result.push([-2, 0], [-1, 0]) if at_start_row?
+    when :black
+      result.push([-2, 0]) if at_start_row?
       result.push([-1, 0])
     end
 
@@ -50,10 +50,29 @@ class Pawn
 
       result << new_pos if on_board?(new_pos) && curr_piece.is_a?(NullPiece)
     end
+    result
   end
 
   def side_attacks
+    result = []
+    sides.each do |dir|
+      dx, dy = dir
+      row, col = pos
+      new_pos = [row + dx, col + dy]
+      curr_piece = board[new_pos]
+      result << new_pos if on_board?(new_pos) && (curr_piece.color != color && !curr_piece.is_a?(NullPiece))
+    end
+    result
 
   end
 
+  def sides
+    dirs_white = [[1, -1], [1, 1]]
+    dirs_black = [[-1, 1], [-1, -1]]
+    if color == :white
+      dirs_white
+    else
+      dirs_black
+    end
+  end
 end
