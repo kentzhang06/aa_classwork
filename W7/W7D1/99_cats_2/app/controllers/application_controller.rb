b1b3024609f8794c0767659fe57326087a11e8c3
Kent Zhang <kentzhang31@yahoo.com>
2021-08-16 15:10:12 -0700

    Application layout view

--- W7/W7D1/99_cats_2/app/controllers/application_controller.rb
+++ W7/W7D1/99_cats_2/app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_user
+    helper_method :current_user, :logged_in?, :logout!
     
     def current_user
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
     def require_logged_in
-
+      redirect_to new_session_url unless logged_in?
     end
 
     def require_logged_out
-
+      redirect_to new_session_url if logged_in?
     end
 
     def login(user)
@@ -19,10 +19,10 @@ class ApplicationController < ActionController::Base
     end
 
     def logged_in?
-
+      !!current_user
     end
 
-    def logout
+    def logout!
         current_user.reset_session_token! if logged_in?
         session[:session_token] = nil
         @current_user = nil
