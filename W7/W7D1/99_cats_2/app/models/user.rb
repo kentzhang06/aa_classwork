c9605471ca171346d683ab7341c6c6199846ec72
Kent Zhang <kentzhang31@yahoo.com>
2021-08-16 13:55:19 -0700

    Created User model validations and methods

---
+++ W7/W7D1/99_cats_2/app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  validates_uniqueness_of :username, :session_token
+  validates_presence_of :username, :password_digest, :session_token
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(username: user_name)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+end
