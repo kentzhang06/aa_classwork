7efe1d20f3af4b5ce5b999d9e157322843f617d4
Kent Zhang <kentzhang31@yahoo.com>
2021-08-26 18:12:07 -0700

    Debounce updated

--- W8/W8D4/intro_callbacks_exercises.js
+++ W8/W8D4/intro_callbacks_exercises.js
@@ -1,100 +1,100 @@
 // Timing is Everything
-// class Clock {
-//   constructor() {
-//     let date = new Date();
-//     this.hours = date.getHours();
-//     this.minutes = date.getMinutes();
-//     this.seconds = date.getSeconds();
-//     this.printTime();
-//     setInterval(this._tick.bind(this), 1000);
-//   }
+class Clock {
+  constructor() {
+    let date = new Date();
+    this.hours = date.getHours();
+    this.minutes = date.getMinutes();
+    this.seconds = date.getSeconds();
+    this.printTime();
+    setInterval(this._tick.bind(this), 1000);
+  }
 
-//   printTime() {
-//     console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
-//   }
+  printTime() {
+    console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+  }
 
-//   _tick() {
-//     this.seconds += 1;
-//     this.printTime();
-//   }
+  _tick() {
+    this.seconds += 1;
+    this.printTime();
+  }
 
-// }
+}
 
 // const clock = new Clock();
 
 //add Numbers
 
-// const readline = require("readline");
-// const reader = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+const readline = require("readline");
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-// function addNumbers(sum, numsLeft, completionCallback) {
-//   if (numsLeft === 0){
-//     reader.close();
-//     return completionCallback(sum);
-//   }
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft === 0){
+    reader.close();
+    return completionCallback(sum);
+  }
 
-//   if (numsLeft > 0) {
-//     reader.question("Enter a number: ", num => {
-//       console.log(`The number you entered is: ${num}`);
-//       sum += parseInt(num);
-//       console.log(`Current sum is ${sum}`);
-//       addNumbers(sum, numsLeft-1, completionCallback);
-//     });
-//   }
-// }
+  if (numsLeft > 0) {
+    reader.question("Enter a number: ", num => {
+      console.log(`The number you entered is: ${num}`);
+      sum += parseInt(num);
+      console.log(`Current sum is ${sum}`);
+      addNumbers(sum, numsLeft-1, completionCallback);
+    });
+  }
+}
 
 // addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
 
 
 // absurdBubbleSort
 
-// function absurdBubbleSort (arr, sortCompletionCallback) {
-//   function outerBubbleSortLoop(madeAnySwaps) {
-//     if (madeAnySwaps){
-//       innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
-//     }else {
-//       sortCompletionCallback(arr);
-//     }
-//   }
+function absurdBubbleSort (arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps){
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    }else {
+      sortCompletionCallback(arr);
+    }
+  }
 
-//   outerBubbleSortLoop(true);
-// }
+  outerBubbleSortLoop(true);
+}
 
-// function askIfGreaterThan (el1, el2, callback) {
-//   console.log(`${el1} > ${el2} `)
-//   reader.question("Enter 'yes' or 'no': ", input => {
-//     console.log(`${input}`);
-//     if (input === 'yes'){
-//       callback(true);
-//     }else {
-//       callback(false);
-//     }
-//   });
-// }
+function askIfGreaterThan (el1, el2, callback) {
+  console.log(`${el1} > ${el2} `)
+  reader.question("Enter 'yes' or 'no': ", input => {
+    console.log(`${input}`);
+    if (input === 'yes'){
+      callback(true);
+    }else {
+      callback(false);
+    }
+  });
+}
 
-// function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
-//   if (i === (arr.length - 1)) {
-//     console.log("i == 0");
-//     return outerBubbleSortLoop(madeAnySwaps);
-//   }
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i === (arr.length - 1)) {
+    console.log("i == 0");
+    return outerBubbleSortLoop(madeAnySwaps);
+  }
 
-//   console.log("if statement")
-//   askIfGreaterThan(arr[i], arr[i+1], function(swap){
-//     console.log("swap");
-//     console.log(swap);
-//     if (swap) {
-//       let temp = arr[i];
-//       arr[i] = arr[i+1];
-//       arr[i+1] = temp;
-//       madeAnySwaps = true;
-//     }
-//     console.log("innerloop");
-//     innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
-//   });
-// }
+  console.log("if statement")
+  askIfGreaterThan(arr[i], arr[i+1], function(swap){
+    console.log("swap");
+    console.log(swap);
+    if (swap) {
+      let temp = arr[i];
+      arr[i] = arr[i+1];
+      arr[i+1] = temp;
+      madeAnySwaps = true;
+    }
+    console.log("innerloop");
+    innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
+  });
+}
 
 // absurdBubbleSort([3, 2, 1], function(arr) {
 //   console.log("Sorted array: " + JSON.stringify(arr));
@@ -103,10 +103,10 @@
 
 // Function Calling
 
-// Function.prototype.myBind = function(context){
-//   debugger;
-//   return () => {this.apply(context)};
-// };
+Function.prototype.myBind = function(context){
+  debugger;
+  return () => {this.apply(context)};
+};
 
 // class Lamp {
 //   constructor() {
@@ -165,40 +165,40 @@ Function.prototype.myThrottle = function(interval) {
 // neuron.fire = neuron.fire.myThrottle(500);
 // neuron.fire('fire');
 
-class SearchBar {
-  constructor() {
-    this.query = "";
+// class SearchBar {
+//   constructor() {
+//     this.query = "";
 
-    this.type = this.type.bind(this);
-    this.search = this.search.bind(this);
-  }
+//     this.type = this.type.bind(this);
+//     this.search = this.search.bind(this);
+//   }
 
-  type(letter) {
-    this.query += letter;
-    this.search();
-  }
+//   type(letter) {
+//     this.query += letter;
+//     this.search();
+//   }
 
-  search() {
-    console.log(`searching for ${this.query}`);
-  }
-}
+//   search() {
+//     console.log(`searching for ${this.query}`);
+//   }
+// }
 
 
-const searchBar = new SearchBar();
+// const searchBar = new SearchBar();
 
-const queryForHelloWorld = () => {
-  searchBar.type("h");
-  searchBar.type("e");
-  searchBar.type("l");
-  searchBar.type("l");
-  searchBar.type("o");
-  searchBar.type(" ");
-  searchBar.type("w");
-  searchBar.type("o");
-  searchBar.type("r");
-  searchBar.type("l");
-  searchBar.type("d");
-};
+// const queryForHelloWorld = () => {
+//   searchBar.type("h");
+//   searchBar.type("e");
+//   searchBar.type("l");
+//   searchBar.type("l");
+//   searchBar.type("o");
+//   searchBar.type(" ");
+//   searchBar.type("w");
+//   searchBar.type("o");
+//   searchBar.type("r");
+//   searchBar.type("l");
+//   searchBar.type("d");
+// };
 
 Function.prototype.myDebounce = function(interval) {
   // debugger;
@@ -222,6 +222,6 @@ Function.prototype.myDebounce = function(interval) {
   // clearTimeout(timeOut);
 }
 
-searchBar.search = searchBar.search.myDebounce(5000);
-searchBar.search();
-queryForHelloWorld();
+// searchBar.search = searchBar.search.myDebounce(5000);
+// searchBar.search();
+// queryForHelloWorld();
