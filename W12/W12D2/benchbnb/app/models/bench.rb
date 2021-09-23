d31183f4d822aebb91494df1c4d45ed797b18a9b
Kent Zhang <kentzhang31@yahoo.com>
2021-09-23 14:13:35 -0700

    Working on inbounds function

--- W12/W12D2/benchbnb/app/models/bench.rb
+++ W12/W12D2/benchbnb/app/models/bench.rb
@@ -1,3 +1,16 @@
 class Bench < ApplicationRecord
   validates_presence_of :description, :lat, :lng
+
+  def self.in_bounds(bounds)
+    # google map bounds will be in the following format:
+    # {
+    #   "northEast"=> {"lat"=>"37.80971", "lng"=>"-122.39208"},
+    #   "southWest"=> {"lat"=>"37.74187", "lng"=>"-122.47791"}
+    # }
+    #... query logic goes here
+    {
+      "northEast"=> {"lat"=>"37.80971", "lng"=>"-122.39208"},
+      "southWest"=> {"lat"=>"37.74187", "lng"=>"-122.47791"}
+    }
+    end
 end
--- W12/W12D2/benchbnb/frontend/util/marker_manager.js
+++ W12/W12D2/benchbnb/frontend/util/marker_manager.js
@@ -6,6 +6,19 @@ export default class MarkerManager {
   }
 
   updateMarkers(benches) {
-    console.log("time to update");
+    benches.forEach((bench) => {
+      this.createMarkerFromBench(bench);
+    });
+  }
+
+  createMarkerFromBench(bench) {
+    if (!this.markers[bench.id]) {
+      const marker = new google.maps.Marker({
+        position: {lat: bench.lat, lng: bench.lng},
+        title: bench.description
+      });
+      marker.setMap(this.map);
+      this.markers[bench.id] = marker;
+    }
   }
 }
