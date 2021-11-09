82a9fe32ec2f1282acf6807e5550411ff35ce081
Kent Zhang <kentzhang31@yahoo.com>
2021-11-09 15:40:37 -0800

    bfs

--- DSA/graph_project/lib/breadth_first_search.js
+++ DSA/graph_project/lib/breadth_first_search.js
@@ -1,4 +1,21 @@
 function breadthFirstSearch(startingNode, targetVal) {
+  //use a queue: fifo
+  //explore all nodes at given depth
+  let queue = [ startingNode ];
+  let visited = new Set();
+
+  while (queue.length) {
+    let curr = queue.shift();
+    // skip node if it has been visited
+    if (visited.has(curr)) continue;
+    // add node if it hasn't been visited
+    visited.add(curr);
+    
+    if (curr.val === targetVal) return curr;
+    queue.push(...curr.neighbors);
+  }
+
+  return null;
 
 }
 
