827d5928a4c12074f22bf9de336d5e93227b5821
Kent Zhang <kentzhang31@yahoo.com>
2021-11-16 14:11:41 -0800

    solution for deleteDuplicates in sorted list

--- DSA/leetcode/remove_duplicates_from_sorted_list.js
+++ DSA/leetcode/remove_duplicates_from_sorted_list.js
@@ -14,3 +14,17 @@ var deleteDuplicates = function (head) {
   }
   return result;
 };
+
+var deleteDuplicates = function (head) {
+  let curr = head;
+
+  while (curr !== null && curr.next !== null) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+
+  return head;
+};
