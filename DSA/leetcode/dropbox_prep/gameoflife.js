5dffbb93d08ffd95be03d73d5a3cb3a3d7246ca0
Kent Zhang <kentzhang31@yahoo.com>
2021-11-30 23:12:51 -0800

    game of life leetcode

--- DSA/leetcode/dropbox_prep/gameoflife.js
+++ DSA/leetcode/dropbox_prep/gameoflife.js
@@ -8,54 +8,48 @@ var gameOfLife = function (board) {
   // iterate cells of the copy board 1 by 1
   // apply rules to original board based on copy of board
 
-  const copy = Array.from(board);
-  let alive;
+  // rules:
+  // live -> die = -1
+  // die -> live = 2
 
-  for (let m = 0; m < copy.length; m++) {
-    for (let n = 0; n < copy[0].length; n++) {
-      alive = countAlive([m, n], copy);
-      console.log(alive);
-      if(copy[m][n] === 1) {
-        if (alive < 2 || alive > 3) {
-          board[m][n] = 0;
-        }else if (alive === 2 || alive === 3) {
-          board[m][n] = 1;
-        }
-      }else if (copy[m][n] === 0){
-        if (alive === 3) board[m][n] = 1;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      let cell = board[i][j];
+      let neighbors = getNeighbors(i, j, board);
+
+      if (cell == 0 && neighbors == 3) {
+        board[i][j] = 2;
+      }
+
+      if (cell == 1 && (neighbors < 2 || neighbors > 3)) {
+        board[i][j] = -1;
       }
     }
   }
 
-  console.log(board);
-  return board;
-
-};
-
-var countAlive = function (pos, board) {
-  let [m, n] = pos;
-  let alive = 0;
-
-  if (inbounds([m-1, n - 1], board) === true && board[m-1][n-1] === 1) alive++;
-  if (inbounds([m-1, n], board) === true && board[m - 1][n] === 1) alive++;
-  if (inbounds([m-1, n + 1], board) === true && board[m - 1][n + 1] === 1) alive++;
-  if (inbounds([m, n - 1], board) === true && board[m][n - 1] === 1) alive++;
-  if (inbounds([m , n + 1], board) === true && board[m][n + 1] === 1) alive++;
-  if (inbounds([m + 1, n - 1], board) === true && board[m + 1][n - 1] === 1) alive++;
-  if (inbounds([m + 1, n], board) === true && board[m + 1][n] === 1) alive++;
-  if (inbounds([m + 1, n + 1], board) === true && board[m + 1][n + 1] === 1) alive++;
-
-  return alive;
-}
-
-var inbounds = function(pos, board) {
-  let [m, n] = pos;
-
-  if ((m < 0 || m > board.length-1) || (n < 0 || n > board[0].length-1)){
-    return false;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] == -1) board[i][j] = 0;
+      if (board[i][j] == 2) board[i][j] = 1;
+    }
   }
 
-  return true;
+  console.log(board);
+};
+
+var getNeighbors = function(r, c, board) {
+  let radius = [-1, 0, +1], count = 0;
+
+  for(let i=0; i < radius.length; i++) {
+    for(let j=0; j < radius.length; j++) {
+      if (!(radius[i] == 0 && radius[j] == 0) && board[r + radius[i]] != null && board[c + radius[j]] != null) {
+        let neighbor = board[r + radius[i]][c + radius[j]];
+        if (Math.abs(neighbor) == 1) count++;
+      }
+    }
+  }
+
+  return count;
 }
 
 let board = [
