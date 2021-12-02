56a30b9b108c291986e22f907f9da6616a463afd
Kent Zhang <kentzhang31@yahoo.com>
2021-12-01 20:55:44 -0800

    game of life

--- DSA/leetcode/dropbox_prep/gameoflife.js
+++ DSA/leetcode/dropbox_prep/gameoflife.js
@@ -4,7 +4,6 @@
 //  */
 
 var gameOfLife = function (board) {
-  // create copy of the board
   // iterate cells of the copy board 1 by 1
   // apply rules to original board based on copy of board
 
@@ -12,45 +11,47 @@ var gameOfLife = function (board) {
   // live -> die = -1
   // die -> live = 2
 
+  // return board if board empty
+  if (!board.length) return board;
+
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
-      let cell = board[i][j];
-      let neighbors = getNeighbors(i, j, board);
-
-      if (cell == 0 && neighbors == 3) {
-        board[i][j] = 2;
-      }
-
-      if (cell == 1 && (neighbors < 2 || neighbors > 3)) {
-        board[i][j] = -1;
+      let score = checkNeighbors(i, j, board);
+      if (board[i][j] === 1) {
+        if (score < 2 || score > 3) {
+          board[i][j] = -0.5;
+        }
+      } else if (board[i][j] === 0) {
+        if (score === 3) {
+          board[i][j] = 0.5;
+        }
       }
     }
   }
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
-      if (board[i][j] == -1) board[i][j] = 0;
-      if (board[i][j] == 2) board[i][j] = 1;
+      board[i][j] = Math.ceil(board[i][j]);
     }
   }
-
-  console.log(board);
 };
 
-var getNeighbors = function(r, c, board) {
-  let radius = [-1, 0, +1], count = 0;
+let checkNeighbors = function (row, col, board) {
+  let score = -board[row][col];
 
-  for(let i=0; i < radius.length; i++) {
-    for(let j=0; j < radius.length; j++) {
-      if (!(radius[i] == 0 && radius[j] == 0) && board[r + radius[i]] != null && board[c + radius[j]] != null) {
-        let neighbor = board[r + radius[i]][c + radius[j]];
-        if (Math.abs(neighbor) == 1) count++;
+  for (let r = row - 1; r <= row + 1; r++) {
+    for (let c = col - 1; c <= col + 1; c++) {
+      if (
+        typeof board[r] !== "undefined" &&
+        typeof board[r][c] !== "undefined"
+      ) {
+        score += Math.abs(Math.floor(board[r][c]));
       }
     }
   }
 
-  return count;
-}
+  return score;
+};
 
 let board = [
   [0, 1, 0],
