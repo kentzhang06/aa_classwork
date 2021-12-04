d97a82ef35127489f03928b566ff2b35142691f6
Kent Zhang <kentzhang31@yahoo.com>
2021-12-03 18:39:28 -0800

    merge two sorted list

---
+++ DSA/leetcode/merge_two_sorted_list.js
@@ -0,0 +1,15 @@
+// O(n + m) time complexity, 
+var mergeTwoLists = function (list1, list2) {
+
+  if (!list1) {
+    return list2;
+  } else if (!list2) {
+    return list1;
+  } else if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list2.next, list1);
+    return list2;
+  }
+};
