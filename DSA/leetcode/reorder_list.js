90e752cfb9e945d12e6b22d788150ec9186b96ce
Kent Zhang <kentzhang31@yahoo.com>
2021-12-10 14:20:36 -0800

    reorder list

---
+++ DSA/leetcode/reorder_list.js
@@ -0,0 +1,31 @@
+var reorderList = function (head) {
+  // hash to keep track of node and its index
+  // iterate through copy of head to populate hash
+  // create new linked list with correct pattern
+  // return new list
+
+  let stack = [];
+  let node = head;
+
+  while (node !== null) {
+    stack.push(node);
+    node = node.next;
+  }
+
+  node = head;
+
+  while (node !== null) {
+    let next = node.next;
+    let endNode = stack.pop();
+
+    node.next = endNode;
+    endNode.next = next;
+    node = next;
+    if (node !== null && node.next === endNode) {
+      node.next = null;
+      break;
+    }
+  }
+
+  return node;
+};
